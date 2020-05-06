db4d307b5865cfffebb68d75715a4eb5e4023122
Josh Ford <thisisjoshford@gmail.com>
2020-05-05 20:55:28 -0700

    fetch repo

--- src/Services/githubAPI.js
+++ src/Services/githubAPI.js
@@ -2,3 +2,8 @@ export const fetchUser = (userName) => {
   return fetch(`https://api.github.com/users/${userName}`)
     .then(res => res.json());
 };
+
+export const fetchRepos = (userName) => {
+  return fetch(`https://api.github.com/users/${userName}/repos`)
+    .then(res => res.json());
+};
